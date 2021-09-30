d0cf64515b8a47d3a799d956d1c93966ccd36bcd
h7ml <h7ml@qq.com>
2021-09-30 10:16:49 +0800

    fix(package.json): remove the interactive information of the automated script
    https://github.com/guardian/configs/compare/jsxBracketSameLine?diff=split
    
    fix https://github.com/dgiot/dgiot-dashboard/issues/136

--- prettier.config.js
+++ prettier.config.js
@@ -8,7 +8,7 @@ module.exports = {
   jsxSingleQuote: false,
   trailingComma: 'es5',
   bracketSpacing: true,
-  jsxBracketSameLine: false,
+  bracketSameLine: false,
   arrowParens: 'always',
   htmlWhitespaceSensitivity: 'ignore',
   vueIndentScriptAndStyle: true,
