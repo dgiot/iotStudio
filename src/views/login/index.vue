70dfe19e2b8f1d8081065f9db60d1eeb5ae609e0
h7ml <h7ml@qq.com>
2021-11-11 16:21:57 +0800

    feat: style

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,7 +2,7 @@
   <div
     ref="container"
     class="login-container"
-    :style="{ backgroundImage: backgroundImage.includes('//')?'url(' +backgroundImage + ')' :'url(' +locationPath+backgroundImage + ')' }"
+    :style="{ backgroundImage:'url(' +backgroundImage + ')'}"
   >
     <el-row>
       <el-col
