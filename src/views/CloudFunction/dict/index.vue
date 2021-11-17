8d1759bcc3ed5c2cfe409a00c28e0e308e2f3803
h7ml <h7ml@qq.com>
2021-11-17 20:08:07 +0800

    perf(src/views/cloudfunction/dict/index.vue): cloudFunctionDict page

---
+++ src/views/CloudFunction/dict/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="cloud-function-dict-container"></div>
+</template>
+
+<script>
+  export default {
+    name: 'CloudFunctionDict',
+    components: {},
+    data() {
+      return {}
+    },
+    created() {},
+    mounted() {},
+    methods: {},
+  }
+</script>
