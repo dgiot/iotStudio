ec70dc0f3420f02e2d191035aece4367dac3a3a6
h7ml <h7ml@qq.com>
2021-12-06 19:31:20 +0800

    chore: src/dgiot/plugins/csi.js

---
+++ src/dgiot/plugins/csi.js
@@ -0,0 +1,22 @@
+/**
+ * @Author: h7ml
+ * @Date: 2021-12-6 19:24:53
+ * @LastEditors: h7ml
+ * @LastEditTime: 2021-12-6 19:24:53
+ * @Description:
+ * @FilePath: src/dgiot/plugins/csi.js
+ * @DocumentLink: https://github.com/tnfe/csijs#2%E4%BD%BF%E7%94%A8
+ */
+import CSI from 'csijs'
+
+// 示例：自定义上报
+const csi = new CSI({
+  feID: 'dgiot',
+  report: (lines) => {
+    // todo 自定义你的上报逻辑
+    console.log('error lins', lines)
+  },
+})
+
+// 如果你想主动上报
+csi.report()
