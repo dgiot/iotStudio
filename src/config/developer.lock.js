e95d601106e89aff8d271edaf1475bc0b63e8164
dext7r <dexter@88.com>
2021-12-23 14:49:56 +0800

    feat(ignore): ignore

---
+++ src/config/developer.lock.js
@@ -0,0 +1,28 @@
+/**
+ * @description 由于dgiot开源平台和项目地址代理api不一致，可以将项目配置文件放在以下。代理链接后面不要写/
+ */
+module.exports = {
+  proxy: [
+    /**
+     * @description iotapi 联调接口。开发使用
+     */
+    {
+      path: 'iotapi',
+      target: 'https://prod.iotn2n.com',
+    },
+    /**
+     * @description 静态资源目录。开发使用
+     */
+    {
+      path: 'dgiot_dashboard',
+      target: 'https://prod.iotn2n.com',
+    },
+    /**
+     * @description amis server 开发使用
+     */
+    {
+      path: 'baidubce',
+      target: 'https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock',
+    },
+  ],
+}
