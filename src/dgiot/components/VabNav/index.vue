4bc0bf83e159f2f3670561a78827f4db2b738b12
dext7r <dexter@88.com>
2021-12-30 15:39:49 +0800

    fix(src/dgiot/components/vabnav/index.vue): loadscript

--- src/dgiot/components/VabNav/index.vue
+++ src/dgiot/components/VabNav/index.vue
@@ -46,9 +46,10 @@
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
   import { openFirstMenu } from '@/config'
-
+  import { mapActions, mapGetters } from 'vuex'
+  import { CDN_URL, proxy, runTimeStatic } from '@/config'
+  import { loadScript } from '@/utils/file/load'
   export default {
     name: 'VabNav',
     props: {
@@ -65,6 +66,7 @@
     },
     computed: {
       ...mapGetters({
+        treeFlag: 'settings/treeFlag',
         mqttName: 'user/username',
         objectId: 'user/objectId',
         extra: 'settings/extra',
@@ -93,6 +95,7 @@
       },
     },
     async mounted() {
+      await that.loadDgiotScript()
       // 写在页面公共组件里。确保全局只订阅一个mqtt。刷新则再次重新订阅
       const md5Info = {
         token: md5(this.token),
@@ -109,7 +112,41 @@
       await this.Mqtt(md5Info)
     },
     methods: {
+      ...mapActions({ setTreeFlag: 'settings/setTreeFlag' }),
+      /**
+       * @Author: dext7r
+       * @Date: 2021-12-30 14:56:41
+       * @LastEditors:
+       * @param
+       * @return {Promise<void>}
+       * @Description:
+       */
+      async loadDgiotScript() {
+        try {
+          const NODE_ENV =
+            process.env.NODE_ENV == 'development'
+              ? proxy[1].target + CDN_URL
+              : CDN_URL
+          console.info('NODE_ENV', NODE_ENV)
+          const staticUrl = NODE_ENV ? `${CDN_URL}/assets/` : '/assets/'
+          var _runTimeStatic = { js: [] }
+          const { js: runTimejs } = runTimeStatic
+          runTimejs.forEach((_js) => {
+            _runTimeStatic.js.push(`${staticUrl}js/${_js}`)
+            _runTimeStatic.js.push(`${staticUrl}css/amis/sdk/sdk.js`)
+          })
+          const res = await loadScript(_runTimeStatic.js)
+          this.$nextTick(async () => {
+            await this.setTreeFlag(!this.treeFlag)
+          })
+          await this.setTreeFlag(!this.treeFlag)
+          console.log(res, 'loadDgiotScript success')
+        } catch (error) {
+          console.error(error, 'loadDgiotScript error')
+        }
+      },
       async Mqtt(md5Info) {
+        console.error('src/dgiot/components/VabNav/index.vue', md5Info)
         const { VUE_APP_URL, NODE_ENV } = process.env
         const { hostname, protocol } = location
         const ip =
