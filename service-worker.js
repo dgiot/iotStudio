b5e13655c4e833f544001ef22404aa3fdac93457
h7ml <h7ml@qq.com>
2022-01-17 04:20:44 +0000

    Deploying to gh-pages from  @ 10bf3d7f99bad80b5e5a6fd5d49aa637654b2053 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.d9c92a77bdb956d9cd7d899b6ef5de94.js"
+  "precache-manifest.ae4943cda5b91b895c949b993d58eaa1.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "@dgiot/dgiot-dashboard"});
