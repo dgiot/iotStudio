d9320d8a40d6dd7ceba52cd8e02e86e0993eda8b
h7ml <h7ml@qq.com>
2022-01-24 15:51:56 +0800

    Deploying to gh-pages from  @ ec83b2412daf3c4037a7331e2f3987b610de97a4 🚀

--- output/assets/js/chunk-1f5f4992.dgiot.js
+++ output/assets/js/chunk-1f5f4992.dgiot.js
@@ -2,7 +2,7 @@
  *  build: 杭州数蛙科技有限公司 
  *  copyright: dgiot-dashboard 
  *  author: h7ml(h7ml@qq.com) 
- *  Time: 1643009571
+ *  Time: 1643010350
  */
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-1f5f4992"],{"0c010":function(e,t,n){
 /*!
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.11afc6b045690d4193b2cad136bb5b31.js"
+  "precache-manifest.9b1cb362461cbf186ca1005c205cd319.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "@dgiot/dgiot-dashboard"});
