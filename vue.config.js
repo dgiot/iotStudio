04ce51e1a34e3ec5f00deaf0717198391d760396
21cm <w4ctech@tom.com>
2022-02-28 20:17:00 +0800

    fix: delete ESBuild

--- vue.config.js
+++ vue.config.js
@@ -161,24 +161,24 @@ function getChainWebpack(config) {
       ])
     }
   })
-  // 使用 esbuild 编译 js 文件
-  const esrule = config.module.rule('js')
-  // 清理自带的 babel-loader
-  esrule.uses.clear()
-  esrule
-    .use('esbuild-loader')
-    .loader('esbuild-loader')
-    .options({
-      loader: 'ts', // 如果使用了 ts, 或者 vue 的 class 装饰器，则需要加上这个 option 配置， 否则会报错：ERROR: Unexpected "@"
-      target: 'es2015',
-      tsconfigRaw: require('./tsconfig.json'),
-    })
-  // 删除底层 terser, 换用 esbuild-minimize-plugin
-  config.optimization.minimizers.delete('terser')
-  // 使用 esbuild 优化 css 压缩
-  config.optimization
-    .minimizer('esbuild')
-    .use(ESBuildMinifyPlugin, [{ minify: true, css: true }])
+  // // 使用 esbuild 编译 js 文件
+  // const esrule = config.module.rule('js')
+  // // 清理自带的 babel-loader
+  // esrule.uses.clear()
+  // esrule
+  //   .use('esbuild-loader')
+  //   .loader('esbuild-loader')
+  //   .options({
+  //     loader: 'ts', // 如果使用了 ts, 或者 vue 的 class 装饰器，则需要加上这个 option 配置， 否则会报错：ERROR: Unexpected "@"
+  //     target: 'es2015',
+  //     tsconfigRaw: require('./tsconfig.json'),
+  //   })
+  // // 删除底层 terser, 换用 esbuild-minimize-plugin
+  // config.optimization.minimizers.delete('terser')
+  // // 使用 esbuild 优化 css 压缩
+  // config.optimization
+  //   .minimizer('esbuild')
+  //   .use(ESBuildMinifyPlugin, [{ minify: true, css: true }])
 }
 
 const resolve = (dir) => {
