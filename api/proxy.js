9791b38b5dedd3a809a8dd119221d5126cefcb47
dgiot-fe <dgiot@foxmail.com>
2022-05-15 12:04:49 +0800

    feat: proxy

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,6 @@
 // 该服务为 vercel serve跨域处理
 const { createProxyMiddleware } = require('http-proxy-middleware')
-const targetUrl = 'https://prod.iotn2n.com'
+const targetUrl = 'https://prod.iotn2n.com/'
 const startsWith = [
   '/iotapi',
   '/dgiot_dashboard',
@@ -11,21 +11,27 @@ const startsWith = [
 module.exports = (req, res) => {
   let target = ''
   // 判断 res.url 开头是否包含startsWith
-  startsWith.forEach((item) => {
-    if (req.url.startsWith(item)) {
-      target = targetUrl
-    }
-  })
+  if (req.url.startsWith('/iotapi')) {
+    target = targetUrl
+  } else if (req.url.startsWith('/dgiot_dashboard')) {
+    target = targetUrl
+  } else if (req.url.includes('/dgiot_swagger')) {
+    target = targetUrl
+  } else if (req.url.startsWith('/dgiot_file')) {
+    target = targetUrl
+  } else if (req.url.includes('/dgiot-amis-dashboard')) {
+    target = targetUrl
+  }
   // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
     changeOrigin: true,
     pathRewrite: {
-      '^/iotapi/': '/iotapi',
-      '^/dgiot_dashboard/': '/dgiot_dashboard',
-      '^/dgiot_swagger/': '/dgiot_swagger',
-      '^/dgiot_file/': '/dgiot_file',
-      '^/dgiot-amis-dashboard/': '/dgiot-amis-dashboard',
+      '^/iotapi/': '/',
+      '^/dgiot_dashboard/': '/',
+      '^/dgiot_swagger/': '/',
+      '^/dgiot_file/': '/',
+      '^/dgiot-amis-dashboard/': '/',
     },
   })(req, res)
 }
