424862d765f93e368821da4fc673e0fb95df4d9c
h7ml <dev@h7ml.cn>
2022-06-14 09:52:55 +0800

    Deploying to www from  @ afa335a61bc2a62acfd17c430e23a954e144060f 🚀

--- output/assets/js/app-dgiot-de478683.dgiot.js
+++ output/assets/js/app-dgiot-de478683.dgiot.js
@@ -2,6 +2,6 @@
  *  build: 杭州数蛙科技有限公司 
  *  copyright: dgiot-dashboard 
  *  author: h7ml(h7ml@qq.com) 
- *  Time: 2022年06月13日14时27分03秒
+ *  Time: 2022年06月14日09时49分54秒
  */
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["app-dgiot-de478683"],{6693:function(o,n,c){o.exports={"menu-color":"rgba(255,255,255,.95)","menu-color-active":"rgba(255,255,255,.95)","menu-background":"#282c34","column-second-menu-background":"#fff"}},b4ff:function(o,n,c){o.exports={"menu-color":"rgba(255,255,255,.95)","menu-color-active":"rgba(255,255,255,.95)","menu-background":"#282c34","column-second-menu-background":"#fff"}}}]);
