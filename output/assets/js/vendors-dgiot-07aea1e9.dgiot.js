3277af8bc8378f301f75d620ffc5413eddf12698
h7ml <dev@h7ml.cn>
2022-06-21 22:46:21 +0800

    Deploying to www from  @ f78274f61300bcda8ab346fa0ef32f76d0180ba7 🚀

--- output/assets/js/vendors-dgiot-07aea1e9.dgiot.js
+++ output/assets/js/vendors-dgiot-07aea1e9.dgiot.js
@@ -2,6 +2,6 @@
  *  build: 杭州数蛙科技有限公司 
  *  copyright: dgiot-dashboard 
  *  author: h7ml(h7ml@qq.com) 
- *  Time: 2022年06月21日10时55分11秒
+ *  Time: 2022年06月21日22时43分33秒
  */
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["vendors-dgiot-07aea1e9"],[]]);
