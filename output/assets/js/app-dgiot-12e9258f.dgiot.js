44de28604f977311a929e7b5cc04df9ee915d980
xuxiaobo1010 <73056200+xuxiaobo1010@users.noreply.github.com>
2022-10-12 13:41:16 +0800

    Deploying to www from  @ 6dd29021b08991a4696207b4d8733d4b52705f02 🚀

--- output/assets/js/app-dgiot-12e9258f.dgiot.js
+++ output/assets/js/app-dgiot-12e9258f.dgiot.js
@@ -2,6 +2,6 @@
  *  build: 杭州数蛙科技有限公司 
  *  copyright: dgiot-dashboard 
  *  author: h7ml(h7ml@qq.com) 
- *  Time: 2022年10月12日12时16分52秒
+ *  Time: 2022年10月12日13时37分41秒
  */
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["app-dgiot-12e9258f"],{"188e":function(o,n,c){o.exports={"menu-color":"rgba(255,255,255,.95)","menu-color-active":"rgba(255,255,255,.95)","menu-background":"#282c34","column-second-menu-background":"#fff"}},"4bba":function(o,n,c){o.exports={"menu-color":"rgba(255,255,255,.95)","menu-color-active":"rgba(255,255,255,.95)","menu-background":"#282c34","column-second-menu-background":"#fff"}},aa43:function(o,n,c){o.exports={"menu-color":"rgba(255,255,255,.95)","menu-color-active":"rgba(255,255,255,.95)","menu-background":"#282c34","column-second-menu-background":"#fff"}}}]);
