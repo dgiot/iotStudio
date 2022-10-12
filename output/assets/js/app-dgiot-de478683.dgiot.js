4dd35ce3dc86b3770cd9f78bde78ff25bac4984f
lsxredrain <34489690@qq.com>
2022-10-12 11:07:29 +0800

    Deploying to www from  @ f6edd53c97a94cd3e7496f68cd4146e6696be64d 🚀

--- output/assets/js/app-dgiot-de478683.dgiot.js
+++ output/assets/js/app-dgiot-de478683.dgiot.js
@@ -2,6 +2,6 @@
  *  build: 杭州数蛙科技有限公司 
  *  copyright: dgiot-dashboard 
  *  author: h7ml(h7ml@qq.com) 
- *  Time: 2022年10月12日11时01分58秒
+ *  Time: 2022年10月12日11时03分29秒
  */
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["app-dgiot-de478683"],{6693:function(o,n,c){o.exports={"menu-color":"rgba(255,255,255,.95)","menu-color-active":"rgba(255,255,255,.95)","menu-background":"#282c34","column-second-menu-background":"#fff"}},b4ff:function(o,n,c){o.exports={"menu-color":"rgba(255,255,255,.95)","menu-color-active":"rgba(255,255,255,.95)","menu-background":"#282c34","column-second-menu-background":"#fff"}}}]);
