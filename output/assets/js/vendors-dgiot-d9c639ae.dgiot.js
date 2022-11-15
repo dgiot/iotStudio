6aa3b8817f2a50da2a2ff00d1f6ad65780e0f147
dawnwinterLiu <47805007+dawnwinterLiu@users.noreply.github.com>
2022-11-15 18:28:41 +0800

    Deploying to www from  @ bb3473e7e03bca740b21cd673507a65b66a6edd6 🚀

--- output/assets/js/vendors-dgiot-d9c639ae.dgiot.js
+++ output/assets/js/vendors-dgiot-d9c639ae.dgiot.js
@@ -2,6 +2,6 @@
  *  build: 杭州数蛙科技有限公司 
  *  copyright: dgiot-dashboard 
  *  author: h7ml(h7ml@qq.com) 
- *  Time: 2022年11月14日21时12分32秒
+ *  Time: 2022年11月15日18时26分00秒
  */
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["vendors-dgiot-d9c639ae"],[]]);
