fa55a1add837237d80a19a85bb00e19e741d3880
xuxiaobo1010 <73056200+xuxiaobo1010@users.noreply.github.com>
2023-01-11 20:45:00 +0800

    Deploying to gh-pages from  @ cd23fe2265b7bdb533375519c36a9a663c6a6a5e 🚀

--- output/assets/js/app-dgiot-91af8647.dgiot.js
+++ output/assets/js/app-dgiot-91af8647.dgiot.js
@@ -2,6 +2,6 @@
  *  build: 杭州数蛙科技有限公司 
  *  copyright: dgiot-dashboard 
  *  author: h7ml(h7ml@qq.com) 
- *  Time: 2023年01月11日20时12分15秒
+ *  Time: 2023年01月11日20时40分40秒
  */
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["app-dgiot-91af8647"],{"2b83":function(o,n,c){o.exports={"menu-color":"rgba(255,255,255,.95)","menu-color-active":"rgba(255,255,255,.95)","menu-background":"#282c34","column-second-menu-background":"#fff"}},6799:function(o,n,c){o.exports={"menu-color":"rgba(255,255,255,.95)","menu-color-active":"rgba(255,255,255,.95)","menu-background":"#282c34","column-second-menu-background":"#fff"}},"7f339":function(o,n,c){o.exports={"menu-color":"rgba(255,255,255,.95)","menu-color-active":"rgba(255,255,255,.95)","menu-background":"#282c34","column-second-menu-background":"#fff"}},"7f48":function(o,n,c){o.exports={"menu-color":"rgba(255,255,255,.95)","menu-color-active":"rgba(255,255,255,.95)","menu-background":"#282c34","column-second-menu-background":"#fff"}},c350:function(o,n,c){o.exports={"menu-color":"rgba(255,255,255,.95)","menu-color-active":"rgba(255,255,255,.95)","menu-background":"#282c34","column-second-menu-background":"#fff"}},ff4f:function(o,n,c){o.exports={"menu-color":"rgba(255,255,255,.95)","menu-color-active":"rgba(255,255,255,.95)","menu-background":"#282c34","column-second-menu-background":"#fff"}}}]);
--- output/assets/js/vendors-dgiot-0a65b932.dgiot.js
+++ output/assets/js/vendors-dgiot-0a65b932.dgiot.js
@@ -2,7 +2,7 @@
  *  build: 杭州数蛙科技有限公司 
  *  copyright: dgiot-dashboard 
  *  author: h7ml(h7ml@qq.com) 
- *  Time: 2023年01月11日20时12分15秒
+ *  Time: 2023年01月11日20时40分40秒
  */
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["vendors-dgiot-0a65b932"],{"2ef0":function(n,t,r){(function(n,e){var u;
 /**
