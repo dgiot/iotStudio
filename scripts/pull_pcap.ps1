217c1e074e376ee26ea6b563838d418138094100
johnliu <34489690@qq.com>
2026-07-11 20:01:00 +0800

    chore: remove test scripts

--- scripts/pull_pcap.ps1
+++
@@ -1,2 +0,0 @@
-Get-ChildItem -Path C:\Users\Administrator\*.pcapng -Recurse -ErrorAction SilentlyContinue | Select FullName,Length
-Get-ChildItem -Path D:\*.pcapng -Recurse -ErrorAction SilentlyContinue | Select FullName,Length
